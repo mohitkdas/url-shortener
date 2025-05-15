7556c810efbe40bc98ee9aee7a2c08d45fef191a
Mohit <mohitds96@gmail.com>
2025-05-16 03:04:04 +0530

    add swagger

--- src/main/java/com/mohit/urlshortener/controller/UrlController.java
+++ src/main/java/com/mohit/urlshortener/controller/UrlController.java
@@ -1,6 +1,10 @@
 package com.mohit.urlshortener.controller;
 
 import com.mohit.urlshortener.service.UrlService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +18,15 @@ import java.io.IOException;
 @RequestMapping
 @AllArgsConstructor
 @Slf4j
+@Tag(name = "URL Shortener", description = "APIs for URL shortening and redirection")
 public class UrlController {
 
     private final UrlService urlService;
 
+    @Operation(summary = "Shorten a long URL", description = "Generate a shortened URL from a long URL")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully shortened the URL")
+    })
     @PostMapping("/api/v1/shorten")
     public ResponseEntity<String> shortenUrl(@RequestBody String longUrl) {
         log.info("Received long URL: {}", longUrl);
@@ -26,6 +35,11 @@ public class UrlController {
         return ResponseEntity.ok("http://54.211.18.213/" + shortCode);
     }
 
+    @Operation(summary = "Redirect to long URL", description = "Redirects the shortened URL to the original long URL")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "302", description = "Successfully redirected"),
+            @ApiResponse(responseCode = "404", description = "Short URL not found")
+    })
     @GetMapping("/{shortCode}")
     public ResponseEntity<Void> redirect(@PathVariable String shortCode, HttpServletResponse response) throws IOException {
         log.info("Redirecting short code: {}", shortCode);
